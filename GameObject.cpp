47a1a6f72c48289c7d1af8edf6945dd62b206833
SasiKoka <borja.sasieta@gmail.com>
2019-01-16 22:26:59 +0100

    update gameObject class
    update this

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,82 +1,126 @@
+#include "Config.h"
 #include "Application.h"
 #include "ModuleScene.h"
+#include "ModuleInput.h"
+#include "ModuleCamera.h"
 #include "ModulePrograms.h"
-#include "ComponentMaterial.h"
 #include "ComponentMesh.h"
+#include "ComponentCamera.h"
+#include "QuadTreeKoka.h"
+#include "ComponentMaterial.h"
 #include "ComponentTransform.h"
-#include "ModuleResourceManager.h"
+#include "ModuleFileSystem.h"
 
 #include "SDL/include/SDL_mouse.h"
 #include "debugdraw.h"
 
+#define MAXNAME 64
+
 GameObject::GameObject() { 
-	uuid = App->resourceManager->NewGuuid();
+	sprintf_s(uuid, App->fileSystem->NewGuuid().str().c_str());
+	transform = (ComponentTransform*)AddComponent(ComponentType::TRANSFORM);
 }
 
-GameObject::GameObject(const char* goName, const math::float4x4& transform, const char* fileLocation) : name(goName) {
+GameObject::GameObject(std::string goName, GameObject* goParent) {
+	sprintf_s(uuid, App->fileSystem->NewGuuid().str().c_str());
 
-	if (fileLocation != nullptr) {
-		filePath = fileLocation;
-	}
-
-	this->parent = App->scene->getRoot();
-	this->transform = (ComponentTransform*)AddComponent(ComponentType::TRANSFORM);
-	this->transform->AddTransform(transform);
-	App->scene->getRoot()->goChilds.push_back(this);
-}
-
-GameObject::GameObject(const char* goName, const math::float4x4& transform, GameObject* goParent, const char* fileLocation) : name(goName) {
-
-	uuid = App->resourceManager->NewGuuid();
+	char* newName = new char[MAXNAME];
+	strcpy(newName, goName.c_str());
+	name = newName;
+	delete[] newName;
 
 	if (goParent != nullptr) {
 		parent = goParent;
-		parentUuid = goParent->uuid;
+		sprintf_s(parentUuid, goParent->uuid);
 		goParent->goChilds.push_back(this);
-	}
-	else {
-		parent = App->scene->getRoot();
-		parentUuid = App->scene->getRoot()->uuid;
-		App->scene->getRoot()->goChilds.push_back(this);
-	}
-
-	this->transform = (ComponentTransform*)AddComponent(ComponentType::TRANSFORM);
-	this->transform->AddTransform(transform);
-
-	if (fileLocation != nullptr) {
-		filePath = fileLocation;
+		staticGo = parent->staticGo;
 	}
 }
 
-GameObject::GameObject(const GameObject& duplicateGameObject) {
+GameObject::GameObject(std::string goName, const math::float4x4& parentTransform) {
+	sprintf_s(uuid, App->fileSystem->NewGuuid().str().c_str());
 
-	uuid = App->resourceManager->NewGuuid();
-	parentUuid = duplicateGameObject.parentUuid;
-	char* copyName = new char[strlen(duplicateGameObject.name)];
-	strcpy(copyName, duplicateGameObject.name);
-	name = copyName;
-	filePath = duplicateGameObject.filePath;
+	char* newName = new char[MAXNAME];
+	strcpy(newName, goName.c_str());
+	name = newName;
+	delete[] newName;
+
+	parent = App->scene->getRoot();
+	sprintf_s(parentUuid, App->scene->getRoot()->uuid);
+	transform = (ComponentTransform*)AddComponent(ComponentType::TRANSFORM);
+	transform->AddTransform(parentTransform);
+	App->scene->getRoot()->goChilds.push_back(this);
+}
+
+GameObject::GameObject(std::string goName, const math::float4x4& parentTransform, GameObject* goParent) {
+	sprintf_s(uuid, App->fileSystem->NewGuuid().str().c_str());
+
+	char* newName = new char[MAXNAME];
+	strcpy(newName, goName.c_str());
+	name = newName;
+	delete[] newName;
+
+	if (goParent != nullptr) {
+		parent = goParent;
+		sprintf_s(parentUuid, goParent->uuid);
+		goParent->goChilds.push_back(this);
+		staticGo = parent->staticGo;
+	}
+	else {
+		parent = App->scene->getRoot();
+		sprintf_s(parentUuid, App->scene->getRoot()->uuid);
+		App->scene->getRoot()->goChilds.push_back(this);
+	}
+
+	transform = (ComponentTransform*)AddComponent(ComponentType::TRANSFORM);
+	transform->AddTransform(parentTransform);
+}
+
+GameObject::GameObject(const GameObject& duplicateGameObject) {
+	sprintf_s(uuid, App->fileSystem->NewGuuid().str().c_str());
+	sprintf_s(parentUuid, duplicateGameObject.parentUuid);
+
+	char* newName = new char[MAXNAME];
+	strcpy(newName, duplicateGameObject.name.c_str());
+	name = newName;
+	delete[] newName;
+
+	staticGo = duplicateGameObject.staticGo;
+	if (staticGo) {
+		UpdateStaticChilds(staticGo);
+	}
 	bbox = duplicateGameObject.bbox;
 
 	for (const auto &component : duplicateGameObject.components) {
 		Component* duplicatedComponent = component->Duplicate();
 		components.push_back(duplicatedComponent);
 		duplicatedComponent->setGoContainer(this);
-		duplicatedComponent->setParentUuid(uuid);
+
+		sprintf_s(duplicatedComponent->parentUuid, uuid);
 		if (duplicatedComponent->getComponentType() == ComponentType::TRANSFORM) {
 			transform = (ComponentTransform*)duplicatedComponent;
+		}
+		if (duplicatedComponent->getComponentType() == ComponentType::MESH) {
+			mesh = (ComponentMesh*)duplicatedComponent;
+		}
+		if (duplicatedComponent->getComponentType() == ComponentType::MATERIAL) {
+			material = (ComponentMaterial*)duplicatedComponent;
 		}
 	}
 
 	for (const auto &child : duplicateGameObject.goChilds) {
 		GameObject* duplicatedChild = new GameObject(*child);
 		duplicatedChild->parent = this;
-		duplicatedChild->parentUuid = uuid;
+		sprintf_s(duplicatedChild->parentUuid, uuid);
 		goChilds.push_back(duplicatedChild);
 	}
 }
 
 GameObject::~GameObject() {
+
+	if (staticGo) {
+		App->scene->quadTree->Remove(this);
+	}
 
 	for (auto &component : components) {
 		delete component;
@@ -88,13 +132,12 @@ GameObject::~GameObject() {
 		delete child;
 		child = nullptr;
 	}
+	goChilds.clear();
 
-	delete transform;
-	transform = nullptr;
-	delete parent;
 	parent = nullptr;
-	delete name;
-	name = nullptr;
+	transform = nullptr;
+	mesh = nullptr;
+	material = nullptr;
 }
 
 void GameObject::Update() {
@@ -104,50 +147,342 @@ void GameObject::Update() {
 
 }
 
-void GameObject::Draw() const {
-
-
-	for (const auto &child : goChilds) {
-		child->Draw();
-	}
+void GameObject::Update() {
 
 	if (!enabled) return;
-	if (transform == nullptr) return;
 
-	ComponentMaterial* material = (ComponentMaterial*)GetComponent(ComponentType::MATERIAL);
-	unsigned shader = 0u;
-	Texture* texture = nullptr;
+	for (std::list<GameObject*>::iterator itChild = goChilds.begin(); itChild != goChilds.end();) {
 
-	if (material != nullptr && material->getEnabled()) {
-		shader = material->GetShader();
-		texture = material->GetTexture();
+		(*itChild)->Update();
+
+		if ((*itChild)->moveGOUp) {
+			(*itChild)->moveGOUp = false;
+			if (std::abs(std::distance(goChilds.begin(), itChild)) != 0) {
+				std::swap(*itChild, *std::prev(itChild));
+			}
+		}
+
+		if ((*itChild)->moveGODown) {
+			(*itChild)->moveGODown = false;
+			if (std::abs(std::distance(goChilds.begin(), itChild)) != goChilds.size() - 1) {
+				std::swap(*itChild, *std::next(itChild));
+			}
+		}
+
+		if ((*itChild)->toBeCopied) {
+			(*itChild)->toBeCopied = false;
+			GameObject* goCopied = new GameObject(**itChild);
+			goCopied->parent = this;
+			goChilds.push_back(goCopied);
+			LOG("Duplicated GO: %s", (*itChild)->name);
+		}
+
+		if ((*itChild)->toBeDeleted) {
+			(*itChild)->toBeDeleted = false;
+			LOG("Removed GO: %s", (*itChild)->name);
+			delete *itChild;
+			goChilds.erase(itChild++);
+		}
+		else {
+			++itChild;
+		}
+
 	}
-	else {
-		shader = App->programs->textureProgram;
+
+}
+
+void GameObject::DrawProperties() {
+
+	if (ImGui::Checkbox("Enabled", &enabled)) {
+		for (auto &component : components) {
+			component->setEnabled(enabled);
+		}
+	} ImGui::SameLine();
+
+	char* newName = new char[MAXNAME];
+	strcpy(newName, name.c_str());
+	ImGui::InputText("Name", (char*)newName, MAXNAME);
+	name = newName;
+	delete[] newName;
+
+
+	if (ImGui::Checkbox("Static", &staticGo)) {
+		UpdateStaticChilds(staticGo);
 	}
 
-	glUseProgram(shader);
-	ModelTransform(shader);
+	if (ImGui::CollapsingHeader("Info")) {
+		ImGui::Text("UUID: "); ImGui::SameLine();
+		ImGui::TextColored({ 0.4f,0.4f,0.4f,1.0f }, uuid);
+		ImGui::Text("Parent UUID: "); ImGui::SameLine();
+		ImGui::TextColored({ 0.4f,0.4f,0.4f,1.0f }, parentUuid);
+	}
 
-	std::vector<Component*> meshes = GetComponents(ComponentType::MESH);
-	for (const auto &mesh : meshes) {
-		if (mesh->getEnabled()) {
-			((ComponentMesh*)mesh)->Draw(shader, texture);
+	if (components.size() > 0) {
+		for (std::list<Component*>::iterator it = components.begin(); it != components.end();) {
+			if (!(*it)->toBeDeleted) {
+				(*it)->DrawProperties(staticGo);
+				it++;
+			}
+			else {
+				it = RemoveComponent(it);
+			}
+		}
+	}
+}
+
+void GameObject::DrawHierarchy(GameObject* goSelected) {
+	ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | (goSelected == this ? ImGuiTreeNodeFlags_Selected : 0);
+
+	ImGui::PushID(this);
+	if (goChilds.empty()) {
+		node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+	}
+
+	bool obj_open = ImGui::TreeNodeEx(this, node_flags, name.c_str());
+
+	if (ImGui::IsItemClicked()) {
+		if (App->scene->getGoSelect() != nullptr) {
+			App->scene->getGoSelect()->drawGOBBox = false;
+		}
+		App->scene->setGoSelected(this);
+		drawGOBBox = true;
+	}
+
+	if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
+		GameObject* draggedGo = this;
+		ImGui::SetDragDropPayload("DragDropHierarchy", &draggedGo, sizeof(GameObject*), ImGuiCond_Once);
+		ImGui::Text("%s", name);
+		ImGui::EndDragDropSource();
+	}
+
+	if (ImGui::BeginDragDropTarget()) {
+		if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("DragDropHierarchy")) {
+			IM_ASSERT(payload->DataSize == sizeof(GameObject*));
+			GameObject* droppedGo = (GameObject*)*(const int*)payload->Data;
+
+			if (droppedGo->parent != this) {
+
+				bool droppedIntoChild = false;
+				GameObject* inheritedTrasnform = this;
+				while (inheritedTrasnform->parent != nullptr) {
+					if (inheritedTrasnform->parent == droppedGo) {
+						droppedIntoChild = true;
+					}
+					inheritedTrasnform = inheritedTrasnform->parent;
+				}
+				inheritedTrasnform = nullptr;
+
+				if (!droppedIntoChild) {
+					goChilds.push_back(droppedGo);
+
+					if (droppedGo->transform != nullptr) {
+						droppedGo->transform->SetLocalToWorld(droppedGo->transform->GetGlobalTransform());
+					}
+					droppedGo->parent->goChilds.remove(droppedGo);
+					droppedGo->parent = this;
+					sprintf_s(droppedGo->parentUuid, uuid);
+					if (droppedGo->transform != nullptr) {
+						droppedGo->transform->SetWorldToLocal(droppedGo->parent->transform->GetGlobalTransform());
+					}
+				}
+
+			}
+		}
+		ImGui::EndDragDropTarget();
+	}
+
+	if (ImGui::IsItemHovered() && App->input->GetMouseButtonDown(SDL_BUTTON_RIGHT) == KEY_DOWN) {
+		ImGui::OpenPopup("Modify_GameObject");
+		App->scene->setGoSelected(this);
+	}
+
+	if (ImGui::BeginPopup("Modify_GameObject")) {
+		if (ImGui::Selectable("Add Empty GameObject")) {
+			App->scene->CreateGameObject(DEFAULT_GO_NAME, this);
+		}
+		if (ImGui::Selectable("Duplicate") && App->scene->getGoSelect() != nullptr) {
+			toBeCopied = true;
+		}
+		if (ImGui::Selectable("Remove") && App->scene->getGoSelect() != nullptr) {
+			toBeDeleted = true;
+			if (App->scene->getGoSelect() == this) {
+				App->scene->setGoSelected(nullptr);
+			}
+		}
+		if (ImGui::Selectable("Move up") && App->scene->getGoSelect() != nullptr) {
+			moveGOUp = true;
+		}
+		if (ImGui::Selectable("Move down") && App->scene->getGoSelect() != nullptr) {
+			moveGODown = true;
+		}
+
+		ImGui::EndPopup();
+	}
+
+	if (obj_open) {
+		for (auto &child : goChilds) {
+			child->DrawHierarchy(goSelected);
+		}
+
+		if (!(node_flags & ImGuiTreeNodeFlags_NoTreePushOnOpen)) {
+			ImGui::TreePop();
+		}
+	}
+	ImGui::PopID();
+}
+
+Component* GameObject::AddComponent(ComponentType type) {
+	Component* component = nullptr;
+
+	switch (type) {
+	case ComponentType::CAMERA:
+		component = new ComponentCamera(this);
+		if (App->camera->getSelectedCamera() == nullptr) {
+			App->camera->setSelectedCamera((ComponentCamera*)component);
+		}
+		App->camera->getGameCameras().push_back((ComponentCamera*)component);
+		break;
+	case ComponentType::TRANSFORM:
+		if (GetComponent(ComponentType::TRANSFORM) == nullptr) {
+			component = new ComponentTransform(this, math::float4x4().identity);
+			transform = (ComponentTransform*)component;
+		}
+		else {
+			LOG("This GO already have a TRANSFORM");
+		}
+		break;
+	case  ComponentType::MESH:
+		if (GetComponent(ComponentType::MESH) == nullptr) {
+			component = new ComponentMesh(this);
+			mesh = (ComponentMesh*)component;
+			//We need to have a material to render a mesh, so we include one if not already added
+			AddComponent(ComponentType::MATERIAL);
+		}
+		else {
+			LOG("This GO already have a MESH");
+		}
+		break;
+	case ComponentType::MATERIAL:
+		if (GetComponent(ComponentType::MATERIAL) == nullptr) {
+			component = new ComponentMaterial(this);
+			material = (ComponentMaterial*)component;
+		}
+		else {
+			LOG("This GO already have a MATERIAL");
+		}
+		break;
+	case ComponentType::EMPTY:
+	default:
+		break;
+	}
+
+	if (component != nullptr) {
+		components.push_back(component);
+	}
+	return component;
+}
+
+std::list<Component*>::iterator GameObject::RemoveComponent(std::list<Component*>::iterator component) {
+	assert(*component != nullptr);
+
+	delete *component;
+	*component = nullptr;
+	return components.erase(component);
+}
+
+Component* GameObject::GetComponent(ComponentType type) const {
+	for (auto &component : components) {
+		if (component->getComponentType() == type) {
+			return component;
 		}
 	}
 
-	glUseProgram(0);
+	return nullptr;
 }
 
-
-void GameObject::DrawProperties() const {
-	assert(name != nullptr);
-
-	ImGui::InputText("Name", (char*)name, sizeof(name));
-
+std::vector<Component*> GameObject::GetComponents(ComponentType type) const {
+	std::vector<Component*> list;
 	for (auto &component : components) {
-		component->DrawProperties();
+		if (component->getComponentType() == type) {
+			list.push_back(component);
+		}
 	}
+
+	return list;
+}
+
+void GameObject::ModelTransform(unsigned shader) const {
+	//TODO: we could probably check if GO have transfom if we want to generate GO without location as Scripts Components, etc.
+	glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_TRUE, &transform->GetGlobalTransform()[0][0]);
+}
+
+void GameObject::UpdateStaticChilds(bool staticState) {
+	staticGo = staticState;
+	if (staticGo && GetComponent(ComponentType::MESH) != nullptr) {
+		App->scene->quadTree->Insert(this, true);
+	}
+	else if (!staticGo && GetComponent(ComponentType::MESH) != nullptr) {
+		App->scene->quadTree->Remove(this);
+	}
+	for (auto &child : goChilds) {
+		child->UpdateStaticChilds(staticState);
+	}
+}
+
+/* RapidJson storage */
+bool GameObject::Save(Config* config) {
+	config->StartObject();
+
+	config->AddString("uuid", uuid);
+	config->AddString("name", name.c_str());
+
+	if (parent != nullptr) {
+		config->AddString("parentUuid", parent->uuid);
+	}
+
+	config->AddBool("enabled", enabled);
+	config->AddBool("static", staticGo);
+
+	config->StartArray("components");
+
+	for (std::list<Component*>::iterator it = components.begin(); it != components.end(); ++it) {
+		(*it)->Save(config);
+	}
+
+	config->EndArray();
+
+	config->EndObject();
+
+	return true;
+}
+
+void GameObject::Load(Config* config, rapidjson::Value& value) {
+	sprintf_s(uuid, config->GetString("uuid", value));
+
+	enabled = config->GetBool("enabled", value);
+	staticGo = config->GetBool("static", value);
+
+	if (parent != nullptr) {
+		sprintf_s(parentUuid, config->GetString("parentUuid", value));
+	}
+	else {
+		sprintf_s(parentUuid, "");
+	}
+
+	rapidjson::Value components = value["components"].GetArray();
+
+	for (rapidjson::Value::ValueIterator it = components.Begin(); it != components.End(); ++it) {
+		Component* component = AddComponent(config->GetComponentType("componentType", (*it)));
+
+		if (component != nullptr) {
+			component->Load(config, (*it));
+		}
+	}
+
+	if (staticGo) {
+		App->scene->quadTree->Insert(this, true);
+	}
+
 }
 
 void GameObject::DrawHierarchy(GameObject* goSelected) {
@@ -176,13 +511,6 @@ void GameObject::DrawHierarchy(GameObject* goSelected) {
 	ImGui::PopID();
 }
 
-std::string GameObject::GetFileFolder() const {
-	std::string s(filePath);
-	std::size_t found = s.find_last_of("/\\");
-	s = s.substr(0, found + 1);
-	return s;
-}
-
 Component* GameObject::AddComponent(ComponentType type) {
 	Component* component = nullptr;
 
@@ -206,19 +534,6 @@ Component* GameObject::AddComponent(ComponentType type) {
 
 	components.push_back(component);
 	return component;
-}
-
-void GameObject::RemoveComponent(Component* component) {
-	assert(component != nullptr);
-
-	for (std::vector<Component*>::iterator it = components.begin(); it != components.end(); ++it) {
-		if ((*it) == component) {
-			components.erase(it);
-			delete component;
-			component = nullptr;
-			return;
-		}
-	}
 }
 
 Component* GameObject::GetComponent(ComponentType type) const {
@@ -256,44 +571,19 @@ void GameObject::ModelTransform(unsigned shader) const {
 	glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_TRUE, &GetGlobalTransform()[0][0]);
 }
 
-AABB& GameObject::ComputeBBox() const {
-	bbox.SetNegativeInfinity();
-
-	// Current GO meshes
-	for (const auto &mesh : GetComponents(ComponentType::MESH)) {
-		bbox.Enclose(((ComponentMesh *)mesh)->getBox());
-	}
-
-	// Apply transformation of our GO
-	bbox.TransformAsAABB(GetGlobalTransform());
-
-	// Child meshes
-	for (const auto &child : goChilds) {
-		if (child->GetComponents(ComponentType::MESH).size() > 0) {
-			bbox.Enclose(child->ComputeBBox());
+AABB& GameObject::ComputeBBox() {
+	for (auto& child : goChilds) {
+		if (child->mesh != nullptr) {
+			child->ComputeBBox();
 		}
 	}
 
+	if (mesh != nullptr && transform != nullptr) {
+		bbox.SetNegativeInfinity();
+		bbox.Enclose(mesh->mesh.bbox);
+		bbox.TransformAsAABB(transform->GetGlobalTransform());
+	}
 	return bbox;
-}
-
-void GameObject::DrawBBox() const {
-
-	math::AABB bbox = ComputeBBox();
-
-	Component* mesh = GetComponent(ComponentType::MESH);
-	if (mesh != nullptr) {
-		dd::aabb(bbox.minPoint, bbox.maxPoint, math::float3(0.0f, 1.0f, 0.0f), true);
-	}
-
-}
-
-void GameObject::CleanUp() {
-
-	for (auto &child : goChilds) {
-		child->CleanUp();
-	}
-
 }
 
 void GameObject::setTransform(ComponentTransform* newTransform) {
--- GameObject.h
+++ GameObject.h
@@ -10,8 +10,13 @@
 #include "Math/float4x4.h"
 #include "Geometry/AABB.h"
 
+#include "document.h"
+#include "prettywriter.h"
 
+class Config;
 class Component;
+class ComponentMesh;
+class ComponentMaterial;
 class ComponentTransform;
 enum class ComponentType;
 
@@ -19,60 +24,72 @@ class GameObject
 {
 public:
 	GameObject();
-	GameObject(const char* goName, const math::float4x4& transform, const char* fileLocation);
-	GameObject(const char* goName, const math::float4x4& transform, GameObject* goParent, const char* fileLocation);
+	GameObject(std::string goName, GameObject* goParent);
+	GameObject(std::string goName, const math::float4x4& transform);
+	GameObject(std::string goName, const math::float4x4& transform, GameObject* goParent);
 	GameObject(const GameObject& duplicateGameObject);
 	~GameObject();
 
 	void Update();
-	void Draw() const;
-	void CleanUp();
-	void DrawProperties() const;
+	void DrawProperties();
 	void DrawHierarchy(GameObject* goSelected);
-	void DrawBBox() const;
-	std::string	GetFileFolder() const;
 
 	Component* AddComponent(ComponentType type);
-	void RemoveComponent(Component* component);
+	std::list<Component*>::iterator RemoveComponent(std::list<Component*>::iterator component);
 	Component* GetComponent(ComponentType type) const;
 	std::vector<Component*> GetComponents(ComponentType type) const;
 
-	AABB& ComputeBBox() const;
+	AABB& ComputeBBox();
 
 	math::float4x4 GetLocalTransform() const;
 	math::float4x4 GetGlobalTransform() const;
 	void ModelTransform(unsigned shader) const;
 
-	const char* getName() { return name; }
+	void UpdateStaticChilds(bool staticState);
+
+	std::string getName() { return name; }
 	void setName(const char* newName) { name = newName; }
 
-	std::vector<Component*>	getComponents() { return components; }
 	std::list<GameObject*> getGoChilds() { return goChilds; }
 
 	ComponentTransform* getTransform() { return transform; }
+	ComponentMesh* getMesh() { return mesh; }
+	ComponentMaterial* getMatertial() { return material; }
+	std::list<Component*> getComponents() { return components; }
 
 	void setTransform(ComponentTransform* newTransform);
 
+	bool Save(Config* config);
+	void Load(Config* config, rapidjson::Value& value);
+
+	bool geToBeDeleted() { return toBeDeleted; }
+	bool getToBeCopied() { return toBeCopied; }
+	bool getMoveGOUp() { return moveGOUp; }
+	bool getMoveGODown() { return moveGODown; }
+	bool getStaticGo() { return staticGo; }
+
 private:
-	std::string uuid = "";
-	std::string	parentUuid = "";
+	char uuid[37];
+	char parentUuid[37];
 	bool enabled = true;
 	bool drawGOBBox = false;
-	bool drawChildsBBox = false;
 	bool duplicating = false;
-	const char* filePath = nullptr;
-	const char*	name = DEFAULT_GO_NAME;
+	std::string	name = std::string(DEFAULT_GO_NAME);
 	GameObject*	parent = nullptr;
-	std::vector<Component*> components;
 	std::list<GameObject*> goChilds;
 
-	ComponentTransform* transform = nullptr;
-	AABB& bbox = AABB();
+	math::AABB bbox;
+
+	ComponentTransform*	transform = nullptr;
+	ComponentMesh* mesh = nullptr;
+	ComponentMaterial* material = nullptr;
+	std::list<Component*> components;
 
 	bool toBeDeleted = false;
 	bool toBeCopied = false;
 	bool moveGOUp = false;
 	bool moveGODown = false;
+	bool staticGo = false;
 };
 
 #endif
